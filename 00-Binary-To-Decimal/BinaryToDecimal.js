bfa587b885906acba9c2e82db972f756f8fa86f0
Paulus <elxebec@gmail.com>
2021-04-25 20:22:08 -0300

    BinaryToDecimal

---
+++ 00-Binary-To-Decimal/BinaryToDecimal.js
@@ -0,0 +1,16 @@
+/**
+ * Escribe una function llamada BinaryToDecimal que acepte string numerico 
+ * y una base.
+ * La funcion deberia retornar un numero entero. 
+ * La idea es tomar un numero binario y "pasarlo" a un numero entero.
+ * La formula para convertir un numero binario en decimal es: N * B ^ I 
+ * Donde N es el numero B es la base e I es el indice.
+ * Digamos que tenemos el binario "101" la formulara seria asi:
+ * 1 * 2 ^ 2  +  0 * 2 ^ 1  +  1 * 2 ^ 0
+
+ * @param {Array} arr1 - esto es un array.
+ * @param {Array} arr1 - esto es un array.
+ * @returns {boolean} - deberia retornar true o false.
+ */
+
+function BinaryToDecimal(binary, base) {}
