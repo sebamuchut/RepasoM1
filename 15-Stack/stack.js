734ece8c6bc797f84624c76c74d1a33e2120d8b2
sebamuchut <sebamuchut@gmail.com>
2021-05-06 08:02:28 -0300

    muchut

--- 15-Stack/stack.js
+++ 15-Stack/stack.js
@@ -1,20 +1,50 @@
-class Node {
-  constructor(value){
+function Node (value){
       this.value = value;
       this.next = null;
-  }
+  
 }
 
-class Stack {
-  constructor(){
+function Stack(){
       this.first = null;
       this.last = null;
       this.size = 0;
   }
-  push(val){
-     
-  }
-  pop(){
+  
+Stack.prototype.push = function (val){
+  var elementonuevo = new Node(val);
+    if (!this.first){
+      this.first = elementonuevo;
+      this.last = elementonuevo;
+      this.size++;
+    }else{
+      var current = this.first;
+      while(current.next){
+        current = current.next;
+      }
+      current.next = elementonuevo;
       
+      this.size++
+    }
+
+}
+
+
+Stack.prototype.pop = function (){
+  if(this.size === 0) return false;
+  if(this.size === 1){
+    this.first = null;
+    this.last = null;
   }
+  if(this.size>1){
+    var current = this.first;
+    var anterior;
+    while(current.next){
+      anterior = current;
+      current=current.next;
+    }
+    anterior.next = null;
+    this.last = anterior;
+    this.size--;
+  }
+  
 }
